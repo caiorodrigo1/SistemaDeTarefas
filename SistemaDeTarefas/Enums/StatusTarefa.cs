0bc4e115ff8f7b04cb91e942bfd906c633c9e787
Caio Santos <98854782+caiorodrigo1@users.noreply.github.com>
2022-11-17 16:29:32 -0300

    Add project files.

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefaEnum
+    {
+        [Description("A fazer")]
+        AFazer = 1,
+        [Description("Em andamento")]
+        EmAndamento = 2,
+        [Description("Concluído")]
+        Concluido = 3,
+    }
+}
---
+++ SistemaDeTarefas/Migrations/20221114113057_FixStatusProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDeTarefas.Migrations
+{
+    public partial class FixStatusProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Tarefas",
+                newName: "StatusTarefa");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StatusTarefa",
+                table: "Tarefas",
+                newName: "Status");
+        }
+    }
+}
